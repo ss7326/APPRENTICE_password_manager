669de9f8d1331890b1d13e39d59c04662d588705
a7uku <htky3131@gmail.com>
2024-12-08 21:49:25 +0900

    done step2

--- step2.sh
+++ step2.sh
@@ -4,9 +4,10 @@ echo "パスワードマネージャーへようこそ！" >&1
 storage_file="./storage/storage.txt"
 
 while true; do
+  echo
   echo "次の選択肢から入力してください(Add Password/Get Password/Exit)：" >&2
   read option
-  echo $option
+
   #
   if [ "$option" = "Add Password" ]; then
     #
@@ -22,8 +23,27 @@ while true; do
     echo $pw_info >>$storage_file
 
   elif [ "$option" = "Get Password" ]; then
-    #
-    echo .
+    echo -n "サービス名を入力してください：" >&2
+    read target_service
+
+    while read line; do
+      service_in_line=$(echo "$line" | cut -d ":" -f1)
+
+      if [ "$target_service" = "$service_in_line" ]; then
+        echo "サービスが見つかりました。"
+        user_name=$(echo "$line" | cut -d ":" -f2)
+        password=$(echo "$line" | cut -d ":" -f3)
+        echo "サービス名：$target_service"
+        echo "ユーザー名：$user_name"
+        echo "パスワード：$password"
+        break
+      fi
+    done <"${storage_file}"
+
+    if [ "$target_service" != "$service_in_line" ]; then
+      echo "そのサービスは登録されていません。"
+    fi
+
   elif [ "$option" = "Exit" ]; then
     #
     echo "Thank you!"
