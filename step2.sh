81debce1dbe8c070fcc40a4b8387d71943c898ba
ss7326 <htky3131@gmail.com>
2024-12-08 21:04:48 +0900

    success to add

--- step2.sh
+++ step2.sh
@@ -1,18 +1,34 @@
 #! /bin/bash
 
 echo "パスワードマネージャーへようこそ！" >&1
-echo -n "サービス名を入力してください：" >&2
-read service
-echo -n "ユーザー名を入力してください：" >&2
-read user_name
-echo -n "パスワードを入力してください：" >&2
-read password
-
-# write file
 storage_file="./storage/storage.txt"
 
-pw_info="${service}:${user_name}:${password}"
-echo $pw_info >>$storage_file
+while true; do
+  echo "次の選択肢から入力してください(Add Password/Get Password/Exit)：" >&2
+  read option
+  echo $option
+  #
+  if [ "$option" = "Add Password" ]; then
+    #
+    echo -n "サービス名を入力してください：" >&2
+    read service
+    echo -n "ユーザー名を入力してください：" >&2
+    read user_name
+    echo -n "パスワードを入力してください：" >&2
+    read password
 
-#
-echo "Thank you!"
+    # write file
+    pw_info="${service}:${user_name}:${password}"
+    echo $pw_info >>$storage_file
+
+  elif [ "$option" = "Get Password" ]; then
+    #
+    echo .
+  elif [ "$option" = "Exit" ]; then
+    #
+    echo "Thank you!"
+    break
+  else
+    echo "入力が間違えています。Add Password/Get Password/Exit から入力してください。"
+  fi
+done
