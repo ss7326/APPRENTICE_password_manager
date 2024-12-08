b418e8e98baed5dd7cbeec100fd0cae2a8316e2e
ss7326 <htky3131@gmail.com>
2024-12-08 20:33:10 +0900

    get std input and echo these.

--- main.sh
+++ main.sh
@@ -1,3 +1,13 @@
 #! /bin/bash
 
 echo "パスワードマネージャーへようこそ！" >&1
+echo -n "サービス名を入力してください：" >&2
+read service
+echo -n "ユーザー名を入力してください：" >&2
+read user_name
+echo -n "パスワードを入力してください：" >&2
+read password
+
+echo $service
+echo $user_name
+echo $password
